9dc4a2e301c7d014b835f823a9174c1bdb9ee6f4
tkachenko <mykhailo.tkachenko@neptis.pl>
2024-11-15 19:57:34 +0100

    Implemented BreaksDao

--- app/src/main/java/ua/salo/abreak/data/local/dao/BreaksDao.kt
+++ app/src/main/java/ua/salo/abreak/data/local/dao/BreaksDao.kt
@@ -1,6 +1,47 @@
 package ua.salo.abreak.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ua.salo.abreak.data.local.entity.BreakEntity
 
+/**
+ * Represents a Data Access Object (DAO) for interacting with the `breaks` table in the database.
+ * This interface defines methods for inserting, retrieving, and querying break data.
+ */
 @Dao
-interface BreaksDao {}
+interface BreaksDao {
+    /**
+     * Inserts a new `BreakEntity` into the database, replacing any existing entry with the same unique identifier.
+     *
+     * @param breakEntity The new `BreakEntity` to be inserted.
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBreak(breakEntity: BreakEntity)
+
+    /**
+     * Retrieves the most recent `BreakEntity` from the database, ordered by timestamp in descending order.
+     *
+     * @return The most recent `BreakEntity`, or null if no breaks exist.
+     */
+    @Query("SELECT * FROM breaks ORDER BY timestamp DESC LIMIT 1")
+    suspend fun getLastBreak(): BreakEntity?
+
+    /**
+     * Retrieves a specific `BreakEntity` from the database based on its position in the ordered list of breaks.
+     *
+     * @param position The zero-based position of the desired break.
+     * @return The `BreakEntity` at the specified position, or null if the position is invalid or no breaks exist.
+     */
+    @Query("SELECT * FROM breaks ORDER BY timestamp ASC LIMIT 1 OFFSET :position")
+    suspend fun getBreakByNumber(position: Int): BreakEntity?
+
+    /**
+     * Retrieves all `BreakEntity` objects from the database, ordered by timestamp in descending order.
+     *
+     * @return A list of all `BreakEntity` objects, or an empty list if no breaks exist.
+     */
+    @Query("SELECT * FROM breaks ORDER BY timestamp DESC")
+    suspend fun getAllBreaks(): List<BreakEntity>?
+}
--- app/src/main/java/ua/salo/abreak/data/local/datasource/BreaksLocalDataSource.kt
+++ app/src/main/java/ua/salo/abreak/data/local/datasource/BreaksLocalDataSource.kt
@@ -1,7 +1,12 @@
 package ua.salo.abreak.data.local.datasource
 
 import ua.salo.abreak.data.local.dao.BreaksDao
+import ua.salo.abreak.data.local.entity.BreakEntity
 
 class BreaksLocalDataSource(
     private val breaksDao: BreaksDao
-) {}
+) {
+    suspend fun insertBreak(breakEntity: BreakEntity) {
+        breaksDao.insertBreak(breakEntity)
+    }
+}
